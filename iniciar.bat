67e7f193703f31c721c938bb6563e83ca0605c70
Joao Igor Paulino Pinto <joaoigorpaulinopinto@gmail.com>
2026-07-30 19:43:22 -0300

    edit iniciar.bat

--- iniciar.bat
+++ iniciar.bat
@@ -1,15 +1,17 @@
 @echo off
 
-start "API" cmd /k "cd /d C:\Users\User\source\multiwhtas-api\multiwhats-api\multiwhats-api && dotnet run"
+set ROOT=%~dp0
+
+start "API" cmd /k "cd /d "%ROOT%multiwhats-api" && dotnet run"
 
 timeout /t 10
 
-start "Messageria" cmd /k "cd /d C:\Users\User\source\multiwhtas-api\multiwhats-api\messageria && npm start"
+start "Messageria" cmd /k "cd /d "%ROOT%messageria" && npm start"
 
 timeout /t 10
 
-start "LegacyDatabaseAdapter" cmd /k "cd /d C:\Users\User\source\multiwhtas-api\multiwhats-api\legacydatabaseadapter && node server.ts"
+start "LegacyDatabaseAdapter" cmd /k "cd /d "%ROOT%legacydatabaseadapter" && node server.ts"
 
 timeout /t 10
 
-start "Front" cmd /k "cd /d C:\Users\User\source\multiwhtas-api\multiwhats-front && npm run dev"
+start "Front" cmd /k "cd /d "%ROOT%..\multiwhats-front" && npm run dev"
